8bd43230ad61c68895b44195ff96f64381183ac3
Anmol Jain <coderanmol25@gmail.com>
2021-09-28 16:22:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser=require("body-parser");
+const date=require(__dirname+"/date.js");
+const app= express();
+var items=["Buy food","Cook food","Eat food"];
+var newWork=[];
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.get("/",function(req,res){
+    
+    var day=date.getDate();
+    res.render("list",{listTitle:day,newListitem:items });
+})
+app.post("/",function(req,res){
+    var item =req.body.newItem;
+     if(req.body.list==="Work")
+     {
+        newWork.push(item);
+        res.redirect("/work");
+     }
+     else
+     {
+        items.push(item);
+        res.redirect("/");
+     }
+})
+app.get("/work",function(req,res){
+    res.render("list",{listTitle:"Work List",newListitem:newWork});
+})
+app.post("/work",function(req,res){
+    var work=req.body.newItem;
+    newWork.push(work);
+    res.redirect("/work");
+})
+app.get("/about",function(req,res){
+    res.render("about");
+})
+app.listen("3000",function(){
+    console.log("Server is on port 3000");
+})
